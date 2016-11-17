390ba1f16898a1a63f26f37a88749618c15e93b0
Jamie Starke <jamies@giftbit.com>
2016-11-17 11:52:04 -0800

    Added an install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+DOWNLOAD_URL="https://raw.githubusercontent.com/jrstarke/aws-profile/80a591d026910171190a179c5e79d63572a366e8/aws-profile"
+DOWNLOAD_SHA="5abcefcacb06f0447772441db6314b7c5ef1f79e6d598afce6a66f6dbb32e270"
+
+mkdir $HOME/bin
+cd $HOME/bin
+
+curl https://raw.githubusercontent.com/jrstarke/aws-profile/master/aws-profile -o aws-profile
+
+if ! echo "$DOWNLOAD_SHA *aws-profile" | shasum -a 256 -c -; then
+	echo "The SHA value did not match the expected"
+	exit 1
+fi
+
+chmod 755 aws-profile
+
+echo "export PATH=$HOME/bin:$PATH" >> $HOME/.bash_profile
+
+echo "aws-profile has been installed."
+echo "You will need to manually run"
+echo ""
+echo "pip install botocore"
